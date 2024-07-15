4d3ed868d07d8742f613207767dbac0cad5587a4
“arturlealbs” <albs@cin.ufpe.br>
2024-07-14 21:01:11 -0300

    tests with face_recognition

--- main.py
+++ main.py
@@ -8,13 +8,12 @@ if __name__ == "__main__":
 
         # Define a suitable threshold for known faces --> 100
         # Define a suitable threshold for face space --> 200
-        # face_recognition = FaceRecognition(training_folder_path, threshold_0=100, threshold_1=200) 
+        face_recognition = FaceRecognition(training_folder_path, threshold_0=10, threshold_1=200) 
 
-        # test_image = cv2.imread(test_image_path, cv2.IMREAD_GRAYSCALE)
-        # identified_face = face_recognition.identify_face(test_image)
+        test_image = cv2.imread(test_image_path, cv2.IMREAD_GRAYSCALE)
+        identified_face = face_recognition.identify_face(test_image)
 
-        print(training_folder_path)
-        print(test_image_path)
+        print(identified_face)
 
     app = FolderImageSelector(execute_callback=execute_face_recognition)
     app.mainloop()
