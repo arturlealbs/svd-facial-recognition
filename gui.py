c50825bc5b64fc7a84c67d3c4345b350db7de31b
“arturlealbs” <albs@cin.ufpe.br>
2024-07-18 13:45:29 -0300

    Display image in the GUI

--- gui.py
+++ gui.py
@@ -1,5 +1,8 @@
+import os
 import tkinter as tk
+import cv2
 from tkinter import filedialog, messagebox
+from PIL import Image, ImageTk
 
 class FolderImageSelector(tk.Tk):
     def __init__(self, execute_callback):
@@ -34,6 +37,10 @@ class FolderImageSelector(tk.Tk):
         # Execute button
         execute_button = tk.Button(self, text="Execute", command=self.execute_function)
         execute_button.grid(row=2, columnspan=3, pady=10)
+
+        # Image display
+        self.image_label = tk.Label(self)
+        self.image_label.grid(row=3, columnspan=3, pady=10)
     
     def select_folder(self):
         folder_path = filedialog.askdirectory()
@@ -51,7 +58,21 @@ class FolderImageSelector(tk.Tk):
         if self.folder_entry.get() and self.image_entry.get():
             self.folder_path = self.folder_entry.get()
             self.image_path = self.image_entry.get()
-            self.execute_callback(self.folder_path, self.image_path)
+            self.execute_callback(self.folder_path, self.image_path, self)
         else:
             messagebox.showwarning("Warning", "Please select both a folder and an image.")
     
+    def show_image_by_id(self, folder_path, image_id):
+
+        image_files = sorted([f for f in os.listdir(folder_path) if f.lower().endswith(('png', 'jpg', 'jpeg', 'bmp'))])
+        
+        if 0 <= image_id < len(image_files):
+            image_path = os.path.join(folder_path, image_files[image_id])
+            img = cv2.imread(image_path)
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  
+            img = cv2.resize(img, (400, 400))  
+            img = Image.fromarray(img)
+            img = ImageTk.PhotoImage(image=img)
+            
+            self.image_label.config(image=img)
+            self.image_label.image = img 
--- main.py
+++ main.py
@@ -4,13 +4,16 @@ from face_recognition import FaceRecognition
 from gui import FolderImageSelector
 
 if __name__ == "__main__":
-    def execute_face_recognition(training_folder_path, test_image_path):
+    def execute_face_recognition(training_folder_path, test_image_path, gui):
         face_recognition = FaceRecognition(training_folder_path, threshold_0=100, threshold_1=500) 
 
         test_image = cv2.imread(test_image_path, cv2.IMREAD_GRAYSCALE)
         identified_face = face_recognition.identify_face(test_image)
 
-        print(identified_face)
+        print(f"Identified face ID: {identified_face}")
+
+        if identified_face is not None:
+            gui.show_image_by_id(training_folder_path, identified_face)
 
     app = FolderImageSelector(execute_callback=execute_face_recognition)
     app.mainloop()
